41b4d3b2385763557a0e071ebc5b00d25e7012ba
Yusei- <acmnhc@gmail.com>
2026-01-20 10:19:49 -0500

    fix:  env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'https://localhost:4000',
+    API_URL: 'https://imagocreations-api.onrender.com',
     PRODUCTION: true,
     CDN_FOLDER: 'images',
     AGENCY_ID: '1c3ac7a0-952b-4321-9f8b-daea20d86e7b'
